4e95aab3ec92e4751b8b099073be80628fe2d393
Sérgio Estêvão <sergio.estevao@smilemachine.com>
2013-11-09 19:21:30 +0000

    Initial Commit

---
+++ SVETabBarController/SVETabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SVETabBarController.h
+//  SVETabBarController
+//
+//  Created by Sérgio Estêvão on 09/11/2013.
+//  Copyright (c) 2013 Sérgio Estêvão. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVETabBarController : NSObject
+
+@end
---
+++ SVETabBarController/SVETabBarController.m
@@ -0,0 +1,13 @@
+//
+//  SVETabBarController.m
+//  SVETabBarController
+//
+//  Created by Sérgio Estêvão on 09/11/2013.
+//  Copyright (c) 2013 Sérgio Estêvão. All rights reserved.
+//
+
+#import "SVETabBarController.h"
+
+@implementation SVETabBarController
+
+@end
